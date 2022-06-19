40fe51673d07edf2d89e6fa5a696b4dd9aac5b9d
Brice Rader <38445592+bricerader@users.noreply.github.com>
2022-06-19 17:31:06 -0400

    Change post creation transition to 'Create Post' button

--- app/routes/posts/admin/new.tsx
+++ app/routes/posts/admin/new.tsx
@@ -68,9 +68,7 @@ export default function NewPost() {
             type="text"
             name="title"
             className={inputClassName}
-            disabled={isCreating}
           />
-          {isCreating ? "Creating..." : "Create Post"}
         </label>
       </p>
       <p>
@@ -106,7 +104,7 @@ export default function NewPost() {
           type="submit"
           className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
         >
-          Create Post
+          {isCreating ? "Creating..." : "Create Post"}
         </button>
       </p>
     </Form>
